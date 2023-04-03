2db0884f838640df806dc69dfb5eddf4d083643e
Tony Creavin <tonycreavin@gmail.com>
2023-04-03 11:27:42 +0200

    francks css reinserted, to be modified

--- src/components/ProfileForm.jsx
+++ src/components/ProfileForm.jsx
@@ -63,144 +63,170 @@ export default function ProfileForm({
             style={{}}
             action=""
           >
-            <div className="col-6">
-              <label htmlFor="firstName">
-                First Name
-                <input
-                  type="text"
-                  className="form form-control"
-                  placeholder="firstname"
-                  value={formState.firstName}
-                  onChange={handleInputChange}
-                  name="firstName"
-                  id="firstName"
-                  style={{ marginBottom: '20px' }}
-                />
-              </label>
-              <label htmlFor="lastName">
-                Lastname
-                <input
-                  type="text"
-                  className="form form-control"
-                  placeholder="lastname"
-                  value={formState.lastName}
-                  onChange={handleInputChange}
-                  name="lastName"
-                  id="lastName"
-                  style={{ marginBottom: '20px' }}
-                />
-              </label>
-              <label htmlFor="phone">
-                Phone
-                <input
-                  type="tel"
-                  className="form form-control"
-                  placeholder="phone"
-                  value={formState.phone}
-                  onChange={handleInputChange}
-                  name="phone"
-                  id="phone"
-                  style={{ marginBottom: '20px' }}
-                />
-              </label>
-              <label htmlFor="github">
-                Github
-                <input
-                  type="text"
-                  className="form form-control"
-                  placeholder="github"
-                  value={formState.github}
-                  onChange={handleInputChange}
-                  name="github"
-                  id="github"
-                  style={{ marginBottom: '20px' }}
-                />
-              </label>
-              <label htmlFor="linkedin">
-                Linkedin
-                <input
-                  type="text"
-                  className="form form-control"
-                  placeholder="linkedin"
-                  value={formState.linkedin}
-                  onChange={handleInputChange}
-                  name="linkedin"
-                  id="linkedin"
-                  style={{ marginBottom: '20px' }}
-                />
-              </label>
-            </div>
-            <div className="col-6">
-              <label htmlFor="skills">
-                Skills
-                <input
-                  type="text"
-                  className="form form-control"
-                  placeholder="skills"
-                  value={formState.skills}
-                  onChange={handleInputChange}
-                  name="skills"
-                  id="skills"
-                  style={{ marginBottom: '20px' }}
-                />
-              </label>
-              <label htmlFor="address">
-                Address
-                <input
-                  type="text"
-                  className="form form-control"
-                  placeholder="address"
-                  value={formState.address}
-                  onChange={handleInputChange}
-                  name="address"
-                  id="address"
-                  style={{ marginBottom: '20px' }}
-                />
-              </label>
-              <label htmlFor="city">
-                City
-                <input
-                  type="text"
-                  className="form form-control"
-                  placeholder="city"
-                  value={formState.city}
-                  onChange={handleInputChange}
-                  name="city"
-                  id="city"
-                  style={{ marginBottom: '20px' }}
-                />
-              </label>
-              <label htmlFor="postcode">
-                Postcode
-                <input
-                  type="text"
-                  placeholder="postcode"
-                  className="form form-control"
-                  value={formState.postcode}
-                  onChange={handleInputChange}
-                  name="postcode"
-                  id="postcode"
-                  style={{ marginBottom: '20px' }}
-                />
-              </label>
-              <label htmlFor="country">
-                Country
-                <input
-                  type="text"
-                  placeholder="country"
-                  className="form form-control"
-                  value={formState.country}
-                  onChange={handleInputChange}
-                  name="country"
-                  id="country"
-                  style={{ marginBottom: '20px' }}
-                />
-              </label>
+            <div className="row my-3">
+              <div className="col-md-6 mb-2 col-sm-12">
+                <label htmlFor="firstName">
+                  First Name
+                  <input
+                    type="text"
+                    className="form form-control"
+                    placeholder="firstname"
+                    value={formState.firstName}
+                    onChange={handleInputChange}
+                    name="firstName"
+                    id="firstName"
+                    style={{ marginBottom: '20px' }}
+                  />
+                </label>
+              </div>
+              <div className="col-md-6 mb-2 col-sm-12">
+                <label htmlFor="lastName">
+                  Lastname
+                  <input
+                    type="text"
+                    className="form form-control"
+                    placeholder="lastname"
+                    value={formState.lastName}
+                    onChange={handleInputChange}
+                    name="lastName"
+                    id="lastName"
+                    style={{ marginBottom: '20px' }}
+                  />
+                </label>
+              </div>
+              <div className="row my-3">
+                <div className="col-md-6 mb-2 col-sm-12">
+                  <label htmlFor="phone">
+                    Phone
+                    <input
+                      type="tel"
+                      className="form form-control"
+                      placeholder="phone"
+                      value={formState.phone}
+                      onChange={handleInputChange}
+                      name="phone"
+                      id="phone"
+                      style={{ marginBottom: '20px' }}
+                    />
+                  </label>
+                </div>
+                <div className="col-md-6 mb-2 col-sm-12">
+                  <label htmlFor="github">
+                    Github
+                    <input
+                      type="text"
+                      className="form form-control"
+                      placeholder="github"
+                      value={formState.github}
+                      onChange={handleInputChange}
+                      name="github"
+                      id="github"
+                      style={{ marginBottom: '20px' }}
+                    />
+                  </label>
+                </div>
+              </div>
+              <div className="row my-3">
+                <div className="col-md-6 mb-2 col-sm-12">
+                  <label htmlFor="linkedin">
+                    Linkedin
+                    <input
+                      type="text"
+                      className="form form-control"
+                      placeholder="linkedin"
+                      value={formState.linkedin}
+                      onChange={handleInputChange}
+                      name="linkedin"
+                      id="linkedin"
+                      style={{ marginBottom: '20px' }}
+                    />
+                  </label>
+                </div>
+                <div className="col-md-6 mb-2 col-sm-12">
+                  <label htmlFor="skills">
+                    Skills
+                    <input
+                      type="text"
+                      className="form form-control"
+                      placeholder="skills"
+                      value={formState.skills}
+                      onChange={handleInputChange}
+                      name="skills"
+                      id="skills"
+                      style={{ marginBottom: '20px' }}
+                    />
+                  </label>
+                </div>
+              </div>
+              <div className="row my-3">
+                <div className="col-md-6 mb-2 col-sm-12">
+                  <label htmlFor="address">
+                    Address
+                    <input
+                      type="text"
+                      className="form form-control"
+                      placeholder="address"
+                      value={formState.address}
+                      onChange={handleInputChange}
+                      name="address"
+                      id="address"
+                      style={{ marginBottom: '20px' }}
+                    />
+                  </label>
+                </div>
+                <div className="col-md-6 mb-2 col-sm-12">
+                  <label htmlFor="city">
+                    City
+                    <input
+                      type="text"
+                      className="form form-control"
+                      placeholder="city"
+                      value={formState.city}
+                      onChange={handleInputChange}
+                      name="city"
+                      id="city"
+                      style={{ marginBottom: '20px' }}
+                    />
+                  </label>
+                </div>
+              </div>
+              <div className="row my-3">
+                <div className="col-md-6 mb-2 col-sm-12">
+                  <label htmlFor="postcode">
+                    Postcode
+                    <input
+                      type="text"
+                      placeholder="postcode"
+                      className="form form-control"
+                      value={formState.postcode}
+                      onChange={handleInputChange}
+                      name="postcode"
+                      id="postcode"
+                      style={{ marginBottom: '20px' }}
+                    />
+                  </label>
+                </div>
+                <div className="col-md-6 mb-2 col-sm-12">
+                  <label htmlFor="country">
+                    Country
+                    <input
+                      type="text"
+                      placeholder="country"
+                      className="form form-control"
+                      value={formState.country}
+                      onChange={handleInputChange}
+                      name="country"
+                      id="country"
+                      style={{ marginBottom: '20px' }}
+                    />
+                  </label>
+                </div>
+              </div>
               <button
                 onClick={handleSubmit}
                 type="submit"
-                className="btn btn-primary btn-block"
-                style={{ width: '70%' }}
+                className="btn btn-primary  mb-3"
+                //style={{ width: '70%' }}
               >
                 envoyer
               </button>
