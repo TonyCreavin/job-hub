b34830603f480c4e7e26d2fdad73d8ba232e6c0b
PROJWEB2 ngfr1805 <ngfr1806@student.iepscf-uccle.be>
2023-04-04 14:42:57 +0200

    SideBar

--- src/pages/Jobseeker.js
+++ src/pages/Jobseeker.js
@@ -9,7 +9,6 @@ const inter = Inter({ subsets: ["latin"] });
 
 const Registration = () => {
   return (
-
     <div className="container">
       <br />
       <br /> <br />
@@ -101,8 +100,7 @@ const Registration = () => {
             Envoyer
           </button>
         </form>
-
-    <div className="w-full h-screen mt-3">
+        <div className="w-full h-screen mt-3"></div>
       </div>
     </div>
   );
