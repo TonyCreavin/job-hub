809783a1e4c916de729fbf2dc0594cd6610baf86
Tony Creavin <tonycreavin@gmail.com>
2023-04-12 16:32:01 +0200

    some css done

--- src/components/EditOffer.jsx
+++ src/components/EditOffer.jsx
@@ -42,95 +42,107 @@ export default function EditOffer({ closeModal, offer }) {
     <div className="w-full mb-10">
       <h3 className="text-center my-5 text-2xl">Edit Offer</h3>
 
-      <form ref={formRef} className="w-[80vw] ml-4">
-        <div className="flex flex-row justify-between">
-          <label className="flex flex-col">
+      <form ref={formRef} className="w-[80vw] mx-auto">
+        <div className="flex flex-col md:flex-row  justify-between  gap-[2vw]">
+          <div className="flex flex-col  mb-2  ">
+            <label htmlFor="editTitle"></label>
             Title
             <input
               defaultValue={offer?.title}
               name="editTitle"
+              id="editTitle"
               type="text"
-              className="border-gray-200 border-2 shadow-lg w-full rounded-md"
+              className="border-solid border-gray-200 border-2  w-[80vw] md:w-[40vw] h-[7vh]  rounded-lg p-3 bg-white "
             />
-          </label>
-
-          <label className="flex flex-col">
+          </div>
+          <div className="flex flex-col  mb-2  ">
+            <label htmlfor="location"> </label>
             Location
             <input
-              className="border-gray-200 border-2 shadow-lg w-full rounded-md"
+              className="border-solid border-gray-200 border-2  w-[80vw] md:w-[40vw] h-[7vh]  rounded-lg p-3 bg-white "
               defaultValue={offer?.location}
               name="editLocation"
               type="text"
+              id="location"
             />
-          </label>
+          </div>
         </div>
-
-        <div className="flex flex-row justify-between">
-          <label className="flex flex-col">
+        <div className="flex flex-col md:flex-row  justify-between gap-[2vw]">
+          <div className="flex flex-col  mb-2  ">
+            <label htmlFor="website"></label>
             Website
             <input
               defaultValue={offer?.website}
               name="editWebsite"
               type="text"
-              className="border-gray-200 border-2 shadow-lg w-full rounded-md"
+              className="border-solid border-gray-200 border-2  w-[80vw] md:w-[40vw] h-[7vh]  rounded-lg p-3 bg-white "
             />
-          </label>
-
-          <label className="flex flex-col">
+          </div>
+          <div className="flex flex-col  mb-2  ">
+            <label htmlFor="skills"></label>
             Skills
             <input
               defaultValue={offer?.skills}
               name="editSkills"
               type="text"
-              className="border-gray-200 border-2 shadow-lg w-full rounded-md"
+              id="skills"
+              className="border-solid border-gray-200 border-2  w-[80vw] md:w-[40vw] h-[7vh]  rounded-lg p-3 bg-white "
             />
-          </label>
+          </div>
         </div>
 
-        <div className="flex flex-row justify-between">
-          <label className="flex flex-col">
-            ContractType
+        <div className="flex flex-col md:flex-row  justify-between gap-[2vw]">
+          <div className="flex flex-col  mb-2  ">
+            <label htmlFor="contractType">ContractType </label>
             <input
               defaultValue={offer?.contractType}
               name="editContractType"
+              id="contractType"
               type="text"
-              className="border-gray-200 border-2 shadow-lg w-full rounded-md"
+              className="border-solid border-gray-200 border-2  w-[80vw] md:w-[40vw] h-[7vh]  rounded-lg p-3 bg-white "
             />
-          </label>
-          <label className="flex flex-col">
-            Company
+          </div>
+          <div className="flex flex-col  mb-2  ">
+            <label htmlFor="company">Company </label>
             <input
-              className="border-gray-200 border-2 shadow-lg w-full rounded-md"
+              className="border-solid border-gray-200 border-2  w-[80vw] md:w-[40vw] h-[7vh]  rounded-lg p-3 bg-white "
               defaultValue={offer?.company}
               name="editCompany"
+              id="company"
               type="text"
             />
-          </label>
+          </div>
         </div>
 
         <div className="inputField">
           <div className="label">
-            <label>Description</label>
+            <label htmlFor="description">Description</label>
           </div>
           <div>
             <textarea
               defaultValue={offer?.description}
               name="editDescription"
               type="text"
-              className="border-gray-200 border-2 shadow-lg  rounded-md w-full h-[100px]"
+              id="description"
+              cols="30"
+              rows="10"
+              className="form form-control"
             ></textarea>
             <label>Company Description</label>
             <textarea
               defaultValue={offer?.companyDescription}
               name="editCompanyDescription"
               type="text"
-              className="border-gray-200 border-2 shadow-lg  rounded-md w-full h-[100px]"
+              id="description"
+              cols="30"
+              rows="10"
+              className="form form-control"
             ></textarea>
           </div>
         </div>
       </form>
 
-      <div className="modal-footer">
+      <div className=" flex flex-row justify-center my-4">
         <button
           className="bg-red-500 ml-4 text-white rounded-md py-1 px-2"
           onClick={() => closeModal()}
