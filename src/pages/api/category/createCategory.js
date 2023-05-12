84cae9a49026461f15f2f7d7c1345855e24fa228
Tony Creavin <tonycreavin@gmail.com>
2023-05-12 15:14:50 +0200

    created page to add categories

--- src/pages/api/category/createCategory.js
+++ src/pages/api/category/createCategory.js
@@ -2,6 +2,7 @@ import prisma from '../../../../lib/prisma';
 
 export default async function handle(req, res) {
   const { name, name_en } = req.body;
+  console.log(`Received category: ${name} (${name_en})`);
   try {
     const category = await prisma.category.create({
       data: {
@@ -9,8 +10,13 @@ export default async function handle(req, res) {
         name_en,
       },
     });
+    console.log(
+      `Category ${category.name} (${category.name_en}) created successfully`
+    );
     res.status(200).json(category);
   } catch (err) {
+    console.error(`Error creating category: ${err.message}`);
+
     res.status(500).json({ message: err.message });
   }
 }
