f1f9c3a26da43204b0fc8dae148fea5ec964b2e2
Tony Creavin <tonycreavin@gmail.com>
2023-05-16 14:48:34 +0200

    error fixed

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -3,8 +3,7 @@ import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import EmailProvider from 'next-auth/providers/email';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { Prisma } from '@prisma/client';
-const prisma = new Prisma();
+import prisma from '../../../../lib/prisma';
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
